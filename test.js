2c8c3ba0d62ef013ec67490ed27a60aadd41c897
johair84 <33111357+johair84@users.noreply.github.com>
2020-08-18 20:22:17 +0600

    templet string

--- test.js
+++ test.js
@@ -1,26 +1,11 @@
-<!-- <h3></h3>
-<pre>
-<code>
-</code> 
-</pre> -->
-
-<h3>Variable</h3>
-<p>let is scope variable.</p>
-<br>
-<h3>Templet String</h3>
-<pre>
-<code>
 const firstName = "Justin";
 const lastName = "TimberLake";
 const fullName = firstName + " " + lastName + " is a boy.";
 //console.log(fullName);
 //output: Justin TimberLake is a boy.
 
+
 //templet string
 const fullName2 = `${firstName} ${lastName} is a boy.`;
 console.log(fullName2);
 //output: Justin TimberLake is a boy.
-</code> 
-</pre>
-
-
