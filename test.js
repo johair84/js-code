c1da91d3c855e1637a931b6b1c39dc5d8e6b3410
johair84 <33111357+johair84@users.noreply.github.com>
2020-08-18 20:29:07 +0600

    fourth try

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 const firstName = "Justin";
 const lastName = "TimberLake";
 const fullName = firstName + " " + lastName + " is a boy.";
-//console.log(fullName);
+console.log(fullName);
 //output: Justin TimberLake is a boy.
-
 
 //templet string
 const fullName2 = `${firstName} ${lastName} is a boy.`;
 console.log(fullName2);
 //output: Justin TimberLake is a boy.
+
